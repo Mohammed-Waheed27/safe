dc57810c3b854117cf2763b8dda532a237281425
mohammed waheed <mohammed.waheed917@gmail.com>
2025-05-19 20:36:15 +0300

    the vedio call main code app and runs

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import 'join_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await permission();
+  await dotenv.load(fileName: ".env");
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: JoinScreen(),
+    );
+  }
+}
+
+Future<void> permission() async {
+  final camerastate = await Permission.camera.request();
+  final micstates = await Permission.microphone.request();
+}
